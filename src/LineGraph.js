1977e67a7f1b8f0abe6f5b598e0871db3df39ae8
Raj varsani <varasaniraj5@gmail.com>
2021-08-12 16:16:26 +0530

    Updated chart

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -2,58 +2,67 @@ import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import numeral from "numeral";
 
-const options = {
-  legend: {
-    display: false,
-  },
-  elements: {
-    point: {
-      radius: 0,
-    },
-  },
-  maintainAspectRatio: false,
-  tooltips: {
-    mode: "index",
-    intersect: false,
-    callbacks: {
-      label: function (tooltipItem, data) {
-        return numeral(tooltipItem.value).format("+0,0");
-      },
-    },
-  },
-  scales: {
-    xAxes: [
-      {
-        type: "time",
-        time: {
-          format: "MM/DD/YY",
-          tooltipFormat: "ll",
-        },
-      },
-    ],
-    yAxes: [
-      {
-        gridLines: {
-          display: false,
-        },
-        ticks: {
-          // Include a dollar sign in the ticks
-          callback: function (value, index, values) {
-            return numeral(value).format("0a");
-          },
-        },
-      },
-    ],
-  },
-};
+// const options = {
+//   legend: {
+//     display: false,
+//   },
+//   elements: {
+//     point: {
+//       radius: 0,
+//     },
+//   },
+//   maintainAspectRatio: false,
+//   tooltips: {
+//     mode: "index",
+//     intersect: false,
+//     callbacks: {
+//       label: function (tooltipItem, data) {
+//         return numeral(tooltipItem.value).format("+0,0");
+//       },
+//     },
+//   },
+//   scales: {
+//     xAxes: [
+//       {
+//         type: "time",
+//         time: {
+//           format: "MM/DD/YY",
+//           tooltipFormat: "ll",
+//         },
+//       },
+//     ],
+//     yAxes: [
+//       {
+//         gridLines: {
+//           display: false,
+//         },
+//         ticks: {
+//           // Include a dollar sign in the ticks
+//           callback: function (value, index, values) {
+//             return numeral(value).format("0a");
+//           },
+//         },
+//       },
+//     ],
+//   },
+// };
 
 const buildChartData = (data, casesType) => {
   let chartData = [0];
- 
+
   let lastDataPoint;
- 
+
+
+  let tempLables = [0];
+  let tempData2 = [0];
+
+
   for (let date in data.cases) {
     if (lastDataPoint) {
+
+      tempLables.push(date);
+      tempData2.push(data.cases[date] - lastDataPoint);
+
       let newDataPoint = {
         x: date,
         y: data.cases[date] - lastDataPoint,
@@ -61,27 +70,38 @@ const buildChartData = (data, casesType) => {
       chartData.push(newDataPoint);
     }
     lastDataPoint = data.cases[date];
-    console.log(lastDataPoint,date);
+    console.log(lastDataPoint, date);
   }
-  return chartData;
+
+
+  console.log(tempLables)
+  // setLables(tempLables);
+  // setData2(tempData2);
+
+  return [chartData, tempLables, tempData2];
 };
 
 
 function LineGraph({ casesType }) {
-  const [data, setData] = useState({});
-  const [data, setData] = useState({});
- 
+  // const [data, setData] = useState({});
+  const [labelsData, setLables] = useState([]);
+  const [secondData, setData2] = useState([]);
+
   useEffect(() => {
-   
+
     const fetchData = async () => {
       await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          let chartData = buildChartData(data, casesType);
-          setData(chartData);
-          console.log(chartData);
+          let [chartData, tempLables, tempData2] = buildChartData(data, casesType);
+          setLables(tempLables);
+          setData2(tempData2);
+          // setData(chartData);
+          console.log(labelsData)
+          console.log(secondData)
+          // console.log(chartData);
           // buildChart(chartData);
         });
     };
@@ -89,36 +109,26 @@ function LineGraph({ casesType }) {
     fetchData();
   }, [casesType]);
 
-  const data2 = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-    datasets: [
-      {
-        label: "First dataset",
-        data: [33, 53, 85, 41, 44, 65 , 45],
-        fill: false,
-        backgroundColor: "rgba(75,192,192,0.2)",
-        borderColor: "rgba(75,192,192,1)"
-      },
-  
-    ]
-  };
 
   return (
     <div>
-      {data?.length > 0 && (
+      {
         <Line
           data={{
+            labels: labelsData,
             datasets: [
               {
-                backgroundColor: "rgba(204, 16, 52, 0.5)",
-                borderColor: "#CC1034",
-                data: data,
+                label: "First dataset",
+                data: secondData,
+                fill: true,
+                backgroundColor: "rgba(75,192,192,0.2)",
+                borderColor: "rgba(75,192,192,1)"
               },
-            ],
-          }}
-          options={options}
+            ]
+          }
+          }
         />
-      )}
+      }
     </div>
   );
 }
